2e260d188419bd6bdcf6e65d4624fa1520480f50
FlyGuy352 <tomlin190@hotmail.com>
2020-12-08 22:43:27 +0800

    first commit

---
+++ src/client/modules/namespace1/app/app.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import Module1 from 'namespace2/module1';
+import Module2 from 'namespace2/module2';
+
+// Uncomment this import and the app crashes
+//import Submodule from 'namespace2/module3/submodule';
+
+export default class App extends LightningElement {}
